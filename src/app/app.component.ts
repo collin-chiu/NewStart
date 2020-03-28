a6cc7d9209cee347f0dce6234e1abcd6ebb4a346
Your Name <you@example.com>
2020-03-29 02:09:55 +0800

    HTTP-1 : read json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { MessageService } from './message.service';
 import { HeroService } from './hero.service';
+import { HttpClient } from '@angular/common/http';
+
+export class Link {
+  heroes: string;
+  dashboard: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,21 +15,26 @@ import { HeroService } from './hero.service';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-
-  get heroesCount$(){
+  link = new Link();
+  get heroesCount$() {
     return this.heroService.getHeroesCount$();
   }
   constructor(
     private messageService: MessageService,
-    private heroService: HeroService
-    ) { }
+    private heroService: HeroService,
+    private http: HttpClient
+  ) { this.getJsonFile() }
 
   onClearTop4Messages() {
     this.messageService.clearTop4Messages();
   }
 
-  onGenMessages(){
+  onGenMessages() {
     this.messageService.genMessages();
+  }
+
+  getJsonFile() {
+    this.http.get('assets/data/dashboard-link.json').subscribe((res:Link) => this.link = res);
   }
 
 }
